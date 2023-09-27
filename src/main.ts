ab40c953de1f3771c4c308b950b844b8ac4cbb49
n1md7 <b.kodua@gmail.com>
2023-09-28 00:05:55 +0300

    chore: mobile style adjusted

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,26 @@ import * as store from '/src/ui/store';
 console.info('Is development mode:', env.isDevelopment());
 
 const game = new Game();
-// @ts-ignore
-window.game = game;
-// @ts-ignore
-window.store = store;
 
 emitter.on('start', async () => {
   game.run(store.mode(), store.stage());
 
   if (env.isCrazyGames()) await window.CrazyGames.SDK.game.gameplayStart();
 });
+
+function isPortrait() {
+  return window.innerWidth < window.innerHeight;
+}
+
+function isMobile() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+function applyMaxHeightToDialog() {
+  if (isMobile() && isPortrait()) {
+    document.documentElement.style.setProperty('--dialog-max-height', '90vh');
+  }
+}
+
+window.addEventListener('resize', applyMaxHeightToDialog);
+window.addEventListener('DOMContentLoaded', applyMaxHeightToDialog);
